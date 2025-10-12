8552e4e98acb1861915ad9980a0f5c3aa2eabd36
jenreh <489879+jenreh@users.noreply.github.com>
2025-10-12 15:37:01 +0200

    refactor: Clean up imports and reset ROLES for users page

--- app/pages/users.py
+++ app/pages/users.py
@@ -1,27 +1,13 @@
 import reflex as rx
-from knai_anonymizer.pages import ANONYMIZER_ROLE
-from knai_avvia import AVVIA_ROLES
-from knai_avvia.components.user_rights import user_rights
-from knai_hours import TEAM_MANAGER_ROLE
-from knai_nmxdia import NMXDIA_ROLES
 
-from manakit_assistant import ASSISTANT_ROLE
-from manakit_imagecreator.pages import IMAGE_GENERATOR_ROLE
-from manakit_ui.authentication.components import requires_admin
-from manakit_ui.authentication.templates import authenticated
 from manakit_ui.components.header import header
-from manakit_ui.user_management.components.user import users_table
+from manakit_user.authentication.components.components import requires_admin
+from manakit_user.authentication.components.templates import authenticated
+from manakit_user.user_management.components.user import users_table
 
 from app.components.navbar import app_navbar
 
-ROLES = [
-    ASSISTANT_ROLE,
-    ANONYMIZER_ROLE,
-    IMAGE_GENERATOR_ROLE,
-    TEAM_MANAGER_ROLE,
-    *AVVIA_ROLES,
-    *NMXDIA_ROLES,
-]
+ROLES = []
 
 
 @authenticated(
@@ -31,9 +17,7 @@ ROLES = [
     admin_only=True,
 )
 def users_page() -> rx.Component:
-    additional_components = [
-        user_rights,
-    ]
+    additional_components = []
 
     return requires_admin(
         rx.vstack(
